4c9bf9c6120b9d35acec9f32340008ebf6cd2eef
Tobias Balle-Petersen <tobiasbp@gmail.com>
2022-09-12 11:24:54 +0200

    Working sqare wave

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-    
+from enum import unique
+from typing import List
+from enum import Enum, auto
+import wave
+import struct
+
+@unique
+class WaveData(Enum):
+    Square50 = 16 * [0] + 16 * [15]
+
+
+class Waveform():
+    """
+    A waveform of length 32
+    """
+
+    def __init__(self, wave_data: WaveData):
+        self._wave: List[int] = wave_data.value
+        self._pos: int = 0
+        self._length = len(self._wave)
+
+        assert self._length == 32
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        """
+        Return the next value in the waveform
+        """
+        v =  self._wave[self._pos]
+        
+        if self._pos == len(self._wave) - 1:
+            self._pos = 0
+        else:
+            self._pos += 1
+        
+        return v
+
+    def __len__(self):
+        return self._length
+
 
 class SquareChannel():
-    # Enevlopes are linear
+    # Envelopes are linear
     samples_pr_cycle = 32
 
-    def __init__(self, freq:int=440, duty_cycle: int = 2):
+    def __init__(self, freq:int=440, duty_cycle: int = 2, sample_rate=44100, buffer_size=256):
         assert duty_cycle in [0,1,2,3]
 
         self.samples_pr_cycle:int=32
@@ -13,6 +54,7 @@ class SquareChannel():
         self.duty_cycle = duty_cycle
         # The waveform
         self.freq = freq
+        self.sample_rate = sample_rate
 
     @property
     def duty_cycle(self):
@@ -44,16 +86,65 @@ class SquareChannel():
     def time_ms(self):
         return self._time_ms
 
-    def sample(self, delta_time):
-        self._time_ms += delta_time
-        step_no = (self._time_ms % self._wave_length_ms) // self.__step_size_ms
-        return self.wave[int(step_no)]
+    def get_samples():
+        """
+        Get the next no_of_sample samples
+        """
+    #def sample(self, delta_time):
+    #    self._time_ms += delta_time
+    #    step_no = (self._time_ms % self._wave_length_ms) // self.__step_size_ms
+    #    return self.wave[int(step_no)]
     
+class Oscillator():
 
-w = SquareChannel(freq=1)
+    def __init__(self, sample_rate=44100, freq=1):
+        self._waveform = Waveform(WaveData.Square50)
+        self._sample_rate = sample_rate
+        # The number of values to get from the wave data pr. sample
+        self._samples_pr_wave_value = sample_rate / (freq * len(self._waveform)) 
+        print("samples_pr_wave_value:", self._samples_pr_wave_value)
 
-print( [ w.sample(1000/32) for i in range(32*1)])
+        assert self._samples_pr_wave_value >= 1.0, f"Sample rate too low. Ratio is: {self._samples_pr_wave_value}"
+        
+        # Current output from oscillator is first value in waveform
+        self._value = next(self._waveform)
+        # Since ratio is not always an int, we need to considet parts of samples
+        self._samples_served: float = 0.0
 
-print("Time passed:", w.time_ms)
-print(w.freq)
-print((440 * 32 * w.step_size_ms))
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # The value to return
+        v = self._value
+
+        self._samples_served += 1.0
+        if self._samples_served >= self._samples_pr_wave_value:
+            # oscillator now has this output
+            self._value = next(self._waveform)
+            # Reset count towards next time we update the output
+            self._samples_served -= self._samples_pr_wave_value
+
+        return v
+        #samples_to_get = n * self._samples_pr_wave_value
+        #print("samples_to_get:", samples_to_get)
+        #return [ next(self._waveform) for i in range(int(samples_to_get)) ]
+
+
+o = Oscillator(sample_rate = 44100, freq=440)
+
+wave_file = wave.open('test.wav', "wb")
+wave_file.setnchannels(1)
+wave_file.setframerate(o.sample_rate)
+wave_file.setsampwidth(1)
+
+for i in range(2 * o.sample_rate):
+    #print([ next(o) for i in range(64)])
+    s = struct.pack('<h', 100 * (next(o) - 8 ))
+    wave_file.writeframesraw(s)
+
+wave_file.close()
